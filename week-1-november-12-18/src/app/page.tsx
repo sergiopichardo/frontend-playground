42e1c5e169b19f57ce1d7f549a25639cfc92f1bc
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2023-11-13 22:53:06 -0600

    [week 1]: add connect message component

---
+++ week-1-november-12-18/src/app/page.tsx
@@ -0,0 +1,9 @@
+import ConnectMessage from '@/components/connect-message'
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-100">
+      <ConnectMessage />
+    </main>
+  )
+}
